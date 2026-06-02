a5713bf5a27c3b07182074df4e768e703bbbab43
Nathan Holmes <contact@nathanholmes.uk>
2026-06-02 09:27:20 +0100

    Create nav.js

---
+++ nav.js
@@ -0,0 +1,17 @@
+// Centralized Navigation Component
+document.addEventListener("DOMContentLoaded", function() {
+    const navHTML = `
+        <nav>
+            <a href="index.html">Home</a> | 
+            <a href="cv.html">CV</a> | 
+            <a href="scorecard.html">Projects</a> |
+            <a href="contact.html">Contact</a>
+        </nav>
+    `;
+    
+    // Inject the navigation into the <header> element of the page
+    const headerElement = document.querySelector('header');
+    if (headerElement) {
+        headerElement.innerHTML = navHTML;
+    }
+});
